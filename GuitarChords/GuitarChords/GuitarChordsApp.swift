032b73a413b3835436288087497b3e9d419ba102
Volodymyr Rykhva <ascentman@ukr.net>
2020-12-21 19:12:28 +0200

    init project

---
+++ GuitarChords/GuitarChords/GuitarChordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarChordsApp.swift
+//  GuitarChords
+//
+//  Created by Volodymyr Rykhva on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GuitarChordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
