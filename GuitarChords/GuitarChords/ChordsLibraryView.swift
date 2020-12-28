fae59113115f63398ec5d50c6f5ac2e6bb7af249
Volodymyr Rykhva <ascentman@ukr.net>
2020-12-28 21:16:50 +0200

    adding first screen ui

--- GuitarChords/GuitarChords/ChordsLibraryView.swift
+++ GuitarChords/GuitarChords/ChordsLibraryView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ChordsLibraryView: View {
     var body: some View {
-        Text("ChordsLibraryView")
+        ZStack {
+            Color("Background")
+            VStack {
+                Rectangle()
+                    .cornerRadius(18)
+                    .frame(width: 240, height: 50)
+                    .padding(40)
+                Image("guitar")
+                Spacer()
+                Rectangle()
+                    .fill(Color("InactiveText"))
+                    .frame(height: 140)
+                    .cornerRadius(18, corners: [.topLeft, .topRight])
+            }
+        }
     }
 }
 
@@ -19,5 +33,22 @@ struct ChordsLibraryView_Previews: PreviewProvider {
             ChordsLibraryView()
                 .navigationBarTitle("Chords library", displayMode: .inline)
         }
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
     }
 }
